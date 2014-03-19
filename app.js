31625bbacf2339ab9727d04e9b32678aa42cb858
huaraco <huaraco@outlook.com>
2014-03-19 11:47:11 +0800

    add carousel

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('cooKeyApp', ['ngRoute','firebase','ngSanitize']);
+var app = angular.module('cooKeyApp', ['ngRoute', 'firebase', 'ngSanitize', 'ui.bootstrap']);
 
 //This configures the routes and associates each route with a view and a controller
 app.config(function ($routeProvider) {
