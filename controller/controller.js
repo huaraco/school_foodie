6ce8a7424a67a7d6bdc5a5ad19f6d3ec66335741
huaraco <huaraco@outlook.com>
2014-03-20 14:18:43 +0800

    update

--- controller/controller.js
+++ controller/controller.js
@@ -153,6 +153,8 @@ app.controller('promotionListController', function ($scope, $routeParams, cooKey
             $scope.promotions = cooKeyService.getStore(storeId);// $firebase(promotionRef);
         }
     }
+
+    $scope.$parent.searchPlaceholder = 'ingredient';
 });
 
 app.controller('ingredientController', function ($scope, cooKeyService) {
